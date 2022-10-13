ecf09e5e87d1e12ae8b4f0149bd78e32e2eb37e6
Farid Attamimi <97972332+Tamimi2022@users.noreply.github.com>
2022-10-13 20:32:43 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ from main import app
 def test_index():
     response = app.test_client().get('/')
     
-    assert response.data == b'Hello, world!'
+    assert response.data == b'Hello, Attamimi!'
     assert response.status_code == 200
